f0f5a707ef8f321a5fd1e3dcbca8c700dd418caa
Marcelo-gms <smarcello023@gmail.com>
2023-07-27 11:29:59 -0400

    resolve problem cors v2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 
 const db = require("./config/db");
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
